4e3b3aaff806a70f18a5908e351aee6aef7e5360
Martin Ovington <MartinOvington@msn.com>
2022-04-28 09:44:51 +0100

    ex8.12 added author selector for editing the birthyear

--- library-frontend/src/components/Authors.js
+++ library-frontend/src/components/Authors.js
@@ -52,10 +52,13 @@ const Authors = (props) => {
       <form onSubmit={submit}>
         <div>
           name
-          <input
-            value={name}
-            onChange={({ target }) => setName(target.value)}
-          />
+          <select value={name} onChange={({ target }) => setName(target.value)}>
+            {authors.map((a) => (
+              <option key={a.name} value={a.name}>
+                {a.name}
+              </option>
+            ))}
+          </select>
         </div>
         <div>
           born
