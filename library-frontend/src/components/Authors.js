1780cbcfb553786dafef0345fb29c6681da4984e
Martin Ovington <MartinOvington@msn.com>
2022-04-28 09:08:21 +0100

    ex8.11 can edit birthyear from author view

--- library-frontend/src/components/Authors.js
+++ library-frontend/src/components/Authors.js
@@ -1,10 +1,26 @@
-import { useQuery } from '@apollo/client'
-import { ALL_AUTHORS } from '../queries'
+import { useState } from 'react'
+import { useQuery, useMutation } from '@apollo/client'
+import { ALL_AUTHORS, EDIT_AUTHOR } from '../queries'
 
 const Authors = (props) => {
+  const [name, setName] = useState('')
+  const [born, setBorn] = useState('')
+
+  const [editAuthor] = useMutation(EDIT_AUTHOR, {
+    refetchQueries: [{ query: ALL_AUTHORS }],
+  })
   const result = useQuery(ALL_AUTHORS)
   if (!props.show) {
     return null
+  }
+
+  const submit = async (event) => {
+    event.preventDefault()
+
+    editAuthor({ variables: { name, setBornTo: parseInt(born) } })
+
+    setName('')
+    setBorn('')
   }
 
   if (result.loading) {
@@ -32,6 +48,25 @@ const Authors = (props) => {
           ))}
         </tbody>
       </table>
+      <h2>Set birthyear</h2>
+      <form onSubmit={submit}>
+        <div>
+          name
+          <input
+            value={name}
+            onChange={({ target }) => setName(target.value)}
+          />
+        </div>
+        <div>
+          born
+          <input
+            type="number"
+            value={born}
+            onChange={({ target }) => setBorn(target.value)}
+          />
+        </div>
+        <button type="submit">update author</button>
+      </form>
     </div>
   )
 }
